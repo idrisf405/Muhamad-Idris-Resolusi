2c329d77397b859d56570eeca7a9a88ea4daa6c7
idrisf405 <50505988+idrisf405@users.noreply.github.com>
2019-05-11 16:05:53 +0700

    Upload File Nomor 5

---
+++ 5.js
@@ -0,0 +1,25 @@
+5. - Koding Mengurutkan Huruf Abjad
+		
+	char awal = 'A';
+	char akhir = 'Z';
+	
+	while (awal <= akhir){
+	 system.out.print(awal + " ");
+
+	}
+
+   - Koding Mencari Nilai Tertinggi
+	var coba = {
+                   ['a','c','b','e','d'],
+		   ['g','e','f'];
+        }
+	
+	sort_array(coba);
+
+   - Koding Mencari Nilai x tertinggi
+	var cobalain = {
+			['d','h','x','j'],
+			['a','c','b','e','d'],
+ 			['q,'w','a'];
+	}
+	sort_array(cobalain);
